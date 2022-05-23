112ec1727560509fd1f4dbaec80cb10ee8e34dc8
Noah <noahmitchell007@gmail.com>
2022-05-22 23:02:12 -0400

    real init

---
+++ Source/BasicFPS/BasicFPS.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BasicFPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BasicFPS, "BasicFPS" );
+ 
