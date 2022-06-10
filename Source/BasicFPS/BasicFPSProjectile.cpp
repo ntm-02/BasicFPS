302cc76a6259db37b957febe2372249c1f78cfd7
root <root@DESKTOP-IEF2NSO.localdomain>
2022-06-09 22:32:38 -0400

    added in a rocket launcher model

--- Source/BasicFPS/BasicFPSProjectile.cpp
+++ Source/BasicFPS/BasicFPSProjectile.cpp
@@ -48,7 +48,7 @@ void ABasicFPSProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor
 	// Only add impulse and destroy projectile if we hit a physics
 	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
 	{
-		//OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
 
 		Destroy();
 	}
